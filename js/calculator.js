caed80bef4622f56d54facdeda49487e4269989e
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-11 23:19:16 +0200

    lunchCalculator

---
+++ js/calculator.js
@@ -0,0 +1,11 @@
+if (document.readyState !== 'loading') {
+    lunchCalculator();
+} else {
+    document.addEventListener('DOMContentLoaded', lunchCalculator);
+}
+
+function lunchCalculator() {
+    let value = 0;
+
+    return value;
+}
