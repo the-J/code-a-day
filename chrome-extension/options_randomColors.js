4d3c98af08daead1c50cc879e8576a5c3f66881a
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-13 22:54:04 +0200

    chrome extensions options page - randomColor generator

---
+++ chrome-extension/options_randomColors.js
@@ -0,0 +1,63 @@
+let page = document.getElementById('buttonDiv');
+
+function randomNumber() {
+    let number = Math.floor((Math.random() * 10));
+    return number === 10 ? "9" : number.toString();
+}
+
+function randomLetter(rand) {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+    return !!letters[rand] ? letters[rand] : letters[10 - rand];
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ *  
+ * Modern version of the Fisher–Yates shuffle algorithm.
+ * 
+ */
+function randomColor(a) {
+    let j, x, i;
+
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+
+    return '#' + a.join('');
+}
+
+function colorOptions() {
+    for (let i = 0; i < 5; i++) {
+
+        let button = document.createElement('button');
+
+        const number = randomNumber();
+        const letter = randomLetter(number);
+
+        let color = '';
+
+        if (i % 2 === 0) {
+            color = randomColor([number, letter, number]);
+        }
+        else {
+            color = randomColor([letter, number, letter]);
+        }
+
+        button.style.backgroundColor = color;
+
+        button.addEventListener('click', function () {
+            chrome.storage.sync.set({ color: color }, function () {
+                console.log('color is: ', color)
+            });
+        });
+
+        page.appendChild(button);
+    }
+}
+
+colorOptions();
