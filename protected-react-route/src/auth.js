0107ee53a4c0745b710724f1ebd15d7f92570258
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-17 09:42:57 +0200

    authorization simulator

---
+++ protected-react-route/src/auth.js
@@ -0,0 +1,24 @@
+/**
+ * Mimicking singleton pattern
+ */
+class Auth {
+    constructor() {
+        this.authenticated = false;
+    }
+
+    login( cb ) {
+        this.authenticated = true;
+        cb();
+    }
+
+    logout( cb ) {
+        this.authenticated = false;
+        cb();
+    }
+
+    isAuthenticated() {
+        return this.authenticated;
+    }
+}
+
+export default new Auth();
