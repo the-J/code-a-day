aada69000ad3501615c4ed90837a72b8ed968ea2
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-17 09:43:18 +0200

    rerouting after login

--- protected-react-route/src/landingPage.js
+++ protected-react-route/src/landingPage.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const LandingPage = () => {
+import auth from './auth';
+
+const LandingPage = (props) => {
     return (
-        <div>Landing Page</div>
+        <div>
+            <h3>Landing Page</h3>
+
+            <button
+                onClick={() => {
+                    auth.login(() => {
+                        props.history.push('/app')
+                    });
+                }}
+            >
+                login
+            </button>
+
+
+        </div>
     );
 };
 
