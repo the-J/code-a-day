c88d0acde11562071ebb0bc9159fbe3a8ff61c03
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-19 11:33:51 +0200

    creating class TwitterStreamer for for streaming and processing live tweets

--- python/tweepy/tweepy_streamer.py
+++ python/tweepy/tweepy_streamer.py
@@ -5,6 +5,29 @@ from tweepy import Stream
 import twitter_credentials
 
 
+# # # # TWITTER STREAMER # # # #
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets
+    """
+
+    def __init__(self):
+        pass
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # this handles twitter authentication and connection to the Twitter Streaming API
+
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+        stream = Stream(auth, listener)
+
+        # This line filter Twitter Streams to capture data by the keywords:
+        stream.filter(track=hash_tag_list)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
