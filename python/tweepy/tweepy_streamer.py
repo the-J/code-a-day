30dbaa43970abb6cdb6fb399b1bfd3137ae328b8
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-19 12:51:09 +0200

    get_friend_list, get_home_timeline_tweets

--- python/tweepy/tweepy_streamer.py
+++ python/tweepy/tweepy_streamer.py
@@ -17,11 +17,21 @@ class TwitterClient:
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
-
         for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
             tweets.append(tweet)
-
         return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+            return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
 
 
 # # # # TWITTER AUTHENTICATOR # # # #
@@ -90,6 +100,9 @@ if __name__ == "__main__":
     fetched_tweets_filename = "tweets.txt"
 
     twitter_client = TwitterClient()
-    print(twitter_client.get_user_timeline_tweets(5))
+    print(twitter_client.get_user_timeline_tweets(1))
+    print(twitter_client.get_friend_list(1))
+    print(twitter_client.get_home_timeline_tweets(1))
+
     # twitter_streamer = TwitterStreamer()
     # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
