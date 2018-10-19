571d3803dc16dfbf693340a92ca3aaecd6288cce
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-10-19 13:20:42 +0200

    getting other values from tweets list

--- python/tweepy/tweepy_streamer.py
+++ python/tweepy/tweepy_streamer.py
@@ -102,12 +102,20 @@ class TwitterListener(StreamListener):
         print(status)
 
 
-class TweetAnalyzer():
+class TweetAnalyzer:
     """
     Functionality for analyzing ad categorizing content from tweets
     """
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
         return df
 
 
@@ -123,6 +131,11 @@ if __name__ == "__main__":
     df = tweet_analyzer.tweets_to_data_frame(tweets)
 
     print(df.head(10))
+
+    """
+    get possible values to write
+    """
+    # print(dir(tweets[0]))
 
     # hash_tag_list = ['barack obama', 'hillary clinton']
     # fetched_tweets_filename = "tweets.txt"
