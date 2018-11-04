8b2690585b5c539d78ba897e2e490f1efc3ba926
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-04 23:31:52 +0100

    init

--- python/snake/snake.py
+++ python/snake/snake.py
@@ -1,6 +1,66 @@
-
 import math
 import random
 import pygame
-inport tinker as tk
-from tinker import messagebox
+import tkinter as tk
+from tkinter import messagebox
+
+
+class cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=1, color=(255.0.0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(selfw, rows, durface):
+    pass
+
+
+def redrawWindow(surface):
+    pass
+
+
+def randomSnack(rows, items):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    pass
+
+
+rows =
+w =
+h =
+
+cube.rows = rows
+cube.w = w
+
+main()
