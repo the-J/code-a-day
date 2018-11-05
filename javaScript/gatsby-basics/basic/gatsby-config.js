f7d974950e94cb12f59681a2586de33341883bf6
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-05 13:20:17 +0100

    extensions and config

--- javaScript/gatsby-basics/basic/gatsby-config.js
+++ javaScript/gatsby-basics/basic/gatsby-config.js
@@ -1,30 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Tinkering with gatsby',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: 'Tinkering with gatsby'
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'tinkering-gatsby',
-        short_name: 'tinkering-gatsby"',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
-  ],
-}
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages/`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'tinkering-gatsby',
+                short_name: 'tinkering-gatsby"',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+            }
+        },
+        'gatsby-plugin-offline',
+        'gatsby-plugin-catch-links',
+        'gatsby-transformer-remark'
+    ]
+};
