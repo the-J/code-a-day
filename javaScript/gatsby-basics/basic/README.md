34d2911ac9056dff4fdf58180801ab1ff914af2b
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-05 12:32:00 +0100

    gatsby default starte with gatsby-cli

---
+++ javaScript/gatsby-basics/basic/README.md
@@ -0,0 +1,1 @@
+## Tinkering in gatsby
---
+++ javaScript/gatsby-basics/basic/gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Tinkering with gatsby',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'tinkering-gatsby',
+        short_name: 'tinkering-gatsby"',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
