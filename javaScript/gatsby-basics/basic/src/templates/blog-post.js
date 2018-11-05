6b4e4b5c5d18efeb641e5cf66aa2edf16e152284
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-05 14:57:04 +0100

    typo and Layout for blog-post

--- javaScript/gatsby-basics/basic/src/templates/blog-post.js
+++ javaScript/gatsby-basics/basic/src/templates/blog-post.js
@@ -5,12 +5,14 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 
+import Layout from '../components/layout';
+
 
 export default function Template( { data } ) {
     const post = data.markdownRemark;
 
     return (
-        <div>
+        <Layout>
             <Link to='/blog'>Go back</Link>
 
             <hr />
@@ -18,8 +20,8 @@ export default function Template( { data } ) {
             <h1>{post.frontmatter.title}</h1>
             <h4>Posted by: {post.frontmatter.author} on {post.frontmatter.date}</h4>
 
-            <div dangerouslySetInnerHTML={{ __html: post.html00 }} />
-        </div>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </Layout>
     );
 }
 
