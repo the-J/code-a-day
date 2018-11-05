59916df6b4628755943703ab2f1d2124db7a74c7
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-05 13:04:28 +0100

    dummy pages for starters

---
+++ javaScript/gatsby-basics/basic/src/pages/about.js
@@ -0,0 +1,15 @@
+/**
+ * Created by juliusz.jakubowski@gmail.com on 05.11.18.
+ */
+
+import React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => (
+    <Layout>
+        <h1>About</h1>
+        <p>About page</p>
+    </Layout>
+);
+
+export default AboutPage;
---
+++ javaScript/gatsby-basics/basic/src/pages/services.js
@@ -0,0 +1,15 @@
+/**
+ * Created by juliusz.jakubowski@gmail.com on 05.11.18.
+ */
+
+import React from 'react';
+import Layout from '../components/layout';
+
+const ServicesPage = () => (
+    <Layout>
+        <h1>Services</h1>
+        <p>Services page</p>
+    </Layout>
+);
+
+export default ServicesPage;
