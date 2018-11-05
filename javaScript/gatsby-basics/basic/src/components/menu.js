90997fb05d4ebdb62fb15860ea2d2474fa92abf4
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-05 13:04:44 +0100

    simple menu component

---
+++ javaScript/gatsby-basics/basic/src/components/menu.js
@@ -0,0 +1,31 @@
+/**
+ * Created by juliusz.jakubowski@gmail.com on 05.11.18.
+ */
+
+import React from 'react';
+import { Link } from 'gatsby';
+
+
+const Menu = () => (
+    <div
+        style={{
+            background: 'grey',
+            paddingTop: '10px'
+        }}
+    >
+        <ul
+            style={{
+                listStyle: 'none',
+                display: 'flex',
+                justifyContent: 'space-evenly'
+            }}
+        >
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/services'>Services</Link></li>
+            <li><Link to='/blog'>Blog</Link></li>
+        </ul>
+    </div>
+);
+
+export default Menu;
