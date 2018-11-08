eb4389ad8515b25b38c4108b8e47e2a60e176b5c
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-08 10:13:39 +0100

    setting ships center

--- javaScript/asteroids-game/asteroids.js
+++ javaScript/asteroids-game/asteroids.js
@@ -7,7 +7,6 @@ const SHIP_SIZE = 30; // ship height in px
 /**type {HTMLCanvasElement} */
 var canv = document.getElementById('gameCanvas');
 var ctx = canv.getContext('2d');
-
 var ship = {
     x: canv.width / 2,
     y: canv.height / 2,
@@ -29,23 +28,28 @@ function update() {
     ctx.beginPath();
 
     ctx.moveTo( // ships upper tip
-        ship.x + ship.r * Math.cos(ship.a),
-        ship.y - ship.r * Math.sin(ship.a)
+        ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+        ship.y - 4 / 3 * ship.r * Math.sin(ship.a)
     );
 
     ctx.lineTo( // ships rear left
-        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
     );
 
     ctx.lineTo( // ships rear right
-        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + Math.cos(ship.a))
     );
 
     ctx.closePath();
     ctx.stroke();
+
+    ctx.fillStyle = 'red'; // center ship dot
+    ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
+
     // rotate ship
 
     //move the ship
+
 }
