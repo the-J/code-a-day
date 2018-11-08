f7c1f457aa99f3ac772beefd5902333b4dde20eb
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-08 09:55:41 +0100

    asteroids - painting the ship

---
+++ javaScript/asteroids-game/asteroids.js
@@ -0,0 +1,51 @@
+/**
+ * Created by juliusz.jakubowski@gmail.com on 08.11.18.
+ */
+
+const FPS = 30; // frames per second
+const SHIP_SIZE = 30; // ship height in px
+/**type {HTMLCanvasElement} */
+var canv = document.getElementById('gameCanvas');
+var ctx = canv.getContext('2d');
+
+var ship = {
+    x: canv.width / 2,
+    y: canv.height / 2,
+    r: SHIP_SIZE / 2,
+    a: 90 / 180 * Math.PI // convert angle to radians
+};
+
+setInterval(update, 1000 / FPS);
+
+function update() {
+    // draw space
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canv.width, canv.height);
+
+    // draw ship
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = SHIP_SIZE / 20;
+
+    ctx.beginPath();
+
+    ctx.moveTo( // ships upper tip
+        ship.x + ship.r * Math.cos(ship.a),
+        ship.y - ship.r * Math.sin(ship.a)
+    );
+
+    ctx.lineTo( // ships rear left
+        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+    );
+
+    ctx.lineTo( // ships rear right
+        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+    );
+
+    ctx.closePath();
+    ctx.stroke();
+    // rotate ship
+
+    //move the ship
+}
