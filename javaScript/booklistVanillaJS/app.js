b608d0c9b8c1438a1777a2b9334ba5f4fa058bb7
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-27 14:27:40 +0100

    creating store in localstorage

--- javaScript/booklistVanillaJS/app.js
+++ javaScript/booklistVanillaJS/app.js
@@ -79,6 +79,35 @@ class UI {
  * Store Class: Handle Storage
  */
 
+class Store {
+    static getBooks() {
+        let books;
+
+        if (localStorage.getItem('books') === null) books = [];
+        else books = JSON.parse(localStorage.getItem('books'));
+
+        return books;
+    }
+
+    static addBook( book ) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook( isbn ) {
+        const books = Store.getBooks();
+
+        books.forEach(( book, index ) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
 /**
  * Event: Display Books
  */
