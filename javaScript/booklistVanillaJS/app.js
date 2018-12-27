d91774c4db917b78f086048b0c857c53c1d20414
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-27 12:47:50 +0100

    adding book to UI - basic

---
+++ javaScript/booklistVanillaJS/app.js
@@ -0,0 +1,84 @@
+/**
+ * Book Class: represents Book
+ */
+class Book {
+    constructor( title, author, isbn ) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+/**
+ * UI Class: Handle UI Tasks
+ */
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '123123'
+            }, {
+                title: 'Book Two',
+                author: 'John Twoooeeee',
+                isbn: '14343433'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach(book => UI.addBookToList(book));
+    }
+
+    static addBookToList( book ) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+/**
+ * Store Class: Handle Storage
+ */
+
+/**
+ * Event: Display Books
+ */
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+/**
+ * Event: Add Book
+ */
+document.querySelector('#book-form').addEventListener('submit', e => {
+    // prevent
+    e.preventDefault();
+
+    // get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Init book
+    const book = new Book(title, author, isbn);
+
+    // Add to UI
+    UI.addBookToList(book);
+});
+
+/**
+ * Event: Remove Book
+ */
+
+
+
+
+
+
