53dbd8a284ea4f2330c2d6ac4e13f3fa42aa00ef
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-28 10:12:19 +0100

    initializing firebase database

--- javaScript/vue/chat-VueFirebase/firebasechat/src/main.js
+++ javaScript/vue/chat-VueFirebase/firebasechat/src/main.js
@@ -1,12 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+// Required for side-effects
+require('firebase/firestore');
+
+const config = {
+    apiKey: 'AIzaSyAS8aJth8l_b_5RnJINsLeJmk78hMOCiUk',
+    authDomain: 'fir-chat-e01e9.firebaseapp.com',
+    databaseURL: 'https://fir-chat-e01e9.firebaseio.com',
+    projectId: 'fir-chat-e01e9',
+    storageBucket: '',
+    messagingSenderId: '57528839553'
+};
+firebase.initializeApp(config);
+
+// Initialize Cloud Firestore through Firebase
+let db = firebase.firestore();
+
+// assign db variable to window to be able to access it from anywhere
+window.db = db;
+
+// Disable deprecated features
+db.settings({
+    timestampsInSnapshots: true
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
