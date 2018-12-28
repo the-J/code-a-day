0207a6e02310549652ae1d8e3b845caf72ac8898
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-28 10:30:37 +0100

    reorganizing code

--- javaScript/vue/chat-VueFirebase/firebasechat/src/views/PrivateChat.vue
+++ javaScript/vue/chat-VueFirebase/firebasechat/src/views/PrivateChat.vue
@@ -10,16 +10,17 @@
                         </div>
                         <div class="srch_bar">
                             <div class="stylish-input-group">
-                                <input type="text" class="search-bar"  placeholder="Search" >
+                                <input type="text" class="search-bar" placeholder="Search">
                                 <span class="input-group-addon">
                 <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
-                </span> </div>
+                </span></div>
                         </div>
                     </div>
                     <div class="inbox_chat">
                         <div class="chat_list active_chat">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -29,7 +30,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -39,7 +41,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -49,7 +52,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -59,7 +63,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -69,7 +74,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -79,7 +85,8 @@
                         </div>
                         <div class="chat_list">
                             <div class="chat_people">
-                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                           alt="sunil"></div>
                                 <div class="chat_ib">
                                     <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
@@ -92,7 +99,8 @@
                 <div class="mesgs">
                     <div class="msg_history">
                         <div class="incoming_msg">
-                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="incoming_msg_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                               alt="sunil"></div>
                             <div class="received_msg">
                                 <div class="received_withd_msg">
                                     <p>Test which is a new approach to have all
@@ -104,10 +112,11 @@
                             <div class="sent_msg">
                                 <p>Test which is a new approach to have all
                                     solutions</p>
-                                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+                                <span class="time_date"> 11:01 AM    |    June 9</span></div>
                         </div>
                         <div class="incoming_msg">
-                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="incoming_msg_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                               alt="sunil"></div>
                             <div class="received_msg">
                                 <div class="received_withd_msg">
                                     <p>Test, which is a new approach to have</p>
@@ -117,10 +126,11 @@
                         <div class="outgoing_msg">
                             <div class="sent_msg">
                                 <p>Apollo University, Delhi, India Test</p>
-                                <span class="time_date"> 11:01 AM    |    Today</span> </div>
+                                <span class="time_date"> 11:01 AM    |    Today</span></div>
                         </div>
                         <div class="incoming_msg">
-                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="incoming_msg_img"><img src="https://ptetutorials.com/images/user-profile.png"
+                                                               alt="sunil"></div>
                             <div class="received_msg">
                                 <div class="received_withd_msg">
                                     <p>We work directly with our designers and suppliers,
@@ -134,7 +144,8 @@
                         <div class="input_msg_write">
                             <input @keyup.enter="saveMessage" v-model="message" type="text" class="write_msg"
                                    placeholder="Type a message" />
-                            <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                            <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o"
+                                                                          aria-hidden="true"></i></button>
                         </div>
                     </div>
                 </div>
@@ -143,7 +154,8 @@
 
             <p class="text-center top_spac"> Design by <a target="_blank" href="#">Sunil Rajput</a></p>
 
-        </div></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -153,7 +165,7 @@
         data() {
             return {
                 message: null
-            }
+            };
         },
 
         methods: {
@@ -161,46 +173,75 @@
                 // save to firestore
                 db.collection('chat').add({
                     message: this.message
-                })
+                });
 
                 this.message = null;
             }
         }
-    }
+    };
 </script>
 
 
 <style scoped="">
-    .container{max-width:1170px; margin:auto;}
-    img{ max-width:100%;}
+    .container {
+        max-width: 1170px;
+        margin: auto;
+    }
+
+    img {
+        max-width: 100%;
+    }
+
     .inbox_people {
         background: #f8f8f8 none repeat scroll 0 0;
         float: left;
         overflow: hidden;
-        width: 40%; border-right:1px solid #c4c4c4;
+        width: 40%;
+        border-right: 1px solid #c4c4c4;
     }
+
     .inbox_msg {
         border: 1px solid #c4c4c4;
         clear: both;
         overflow: hidden;
     }
-    .top_spac{ margin: 20px 0 0;}
+
+    .top_spac {
+        margin: 20px 0 0;
+    }
 
 
-    .recent_heading {float: left; width:40%;}
+    .recent_heading {
+        float: left;
+        width: 40%;
+    }
+
     .srch_bar {
         display: inline-block;
         text-align: right;
         width: 60%;
     }
-    .headind_srch{ padding:10px 29px 10px 20px; overflow:hidden; border-bottom:1px solid #c4c4c4;}
+
+    .headind_srch {
+        padding: 10px 29px 10px 20px;
+        overflow: hidden;
+        border-bottom: 1px solid #c4c4c4;
+    }
 
     .recent_heading h4 {
         color: #05728f;
         font-size: 21px;
         margin: auto;
     }
-    .srch_bar input{ border:1px solid #cdcdcd; border-width:0 0 1px 0; width:80%; padding:2px 0 4px 6px; background:none;}
+
+    .srch_bar input {
+        border: 1px solid #cdcdcd;
+        border-width: 0 0 1px 0;
+        width: 80%;
+        padding: 2px 0 4px 6px;
+        background: none;
+    }
+
     .srch_bar .input-group-addon button {
         background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
         border: medium none;
@@ -208,41 +249,71 @@
         color: #707070;
         font-size: 18px;
     }
-    .srch_bar .input-group-addon { margin: 0 0 0 -27px;}
 
-    .chat_ib h5{ font-size:15px; color:#464646; margin:0 0 8px 0;}
-    .chat_ib h5 span{ font-size:13px; float:right;}
-    .chat_ib p{ font-size:14px; color:#989898; margin:auto}
+    .srch_bar .input-group-addon {
+        margin: 0 0 0 -27px;
+    }
+
+    .chat_ib h5 {
+        font-size: 15px;
+        color: #464646;
+        margin: 0 0 8px 0;
+    }
+
+    .chat_ib h5 span {
+        font-size: 13px;
+        float: right;
+    }
+
+    .chat_ib p {
+        font-size: 14px;
+        color: #989898;
+        margin: auto
+    }
+
     .chat_img {
         float: left;
         width: 11%;
     }
+
     .chat_ib {
         float: left;
         padding: 0 0 0 15px;
         width: 88%;
     }
 
-    .chat_people{ overflow:hidden; clear:both;}
+    .chat_people {
+        overflow: hidden;
+        clear: both;
+    }
+
     .chat_list {
         border-bottom: 1px solid #c4c4c4;
         margin: 0;
         padding: 18px 16px 10px;
     }
-    .inbox_chat { height: 550px; overflow-y: scroll;}
 
-    .active_chat{ background:#ebebeb;}
+    .inbox_chat {
+        height: 550px;
+        overflow-y: scroll;
+    }
+
+    .active_chat {
+        background: #ebebeb;
+    }
 
     .incoming_msg_img {
         display: inline-block;
         width: 6%;
     }
+
     .received_msg {
         display: inline-block;
         padding: 0 0 0 10px;
         vertical-align: top;
         width: 92%;
     }
+
     .received_withd_msg p {
         background: #ebebeb none repeat scroll 0 0;
         border-radius: 3px;
@@ -252,13 +323,18 @@
         padding: 5px 10px 5px 12px;
         width: 100%;
     }
+
     .time_date {
         color: #747474;
         display: block;
         font-size: 12px;
         margin: 8px 0 0;
     }
-    .received_withd_msg { width: 57%;}
+
+    .received_withd_msg {
+        width: 57%;
+    }
+
     .mesgs {
         float: left;
         padding: 30px 15px 0 25px;
@@ -269,15 +345,22 @@
         background: #05728f none repeat scroll 0 0;
         border-radius: 3px;
         font-size: 14px;
-        margin: 0; color:#fff;
+        margin: 0;
+        color: #fff;
         padding: 5px 10px 5px 12px;
-        width:100%;
+        width: 100%;
     }
-    .outgoing_msg{ overflow:hidden; margin:26px 0 26px;}
+
+    .outgoing_msg {
+        overflow: hidden;
+        margin: 26px 0 26px;
+    }
+
     .sent_msg {
         float: right;
         width: 46%;
     }
+
     .input_msg_write input {
         background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
         border: medium none;
@@ -287,7 +370,11 @@
         width: 100%;
     }
 
-    .type_msg {border-top: 1px solid #c4c4c4;position: relative;}
+    .type_msg {
+        border-top: 1px solid #c4c4c4;
+        position: relative;
+    }
+
     .msg_send_btn {
         background: #05728f none repeat scroll 0 0;
         border: medium none;
@@ -301,7 +388,11 @@
         top: 11px;
         width: 33px;
     }
-    .messaging { padding: 0 0 50px 0;}
+
+    .messaging {
+        padding: 0 0 50px 0;
+    }
+
     .msg_history {
         height: 516px;
         overflow-y: auto;
