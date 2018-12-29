be2781fd8284a0470688eb374466c23ed669fcae
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-29 15:13:36 +0100

    overwrite default colors

--- vue/vuetify/src/plugins/vuetify.js
+++ vue/vuetify/src/plugins/vuetify.js
@@ -3,5 +3,12 @@ import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
-    iconfont: 'md'
+    iconfont: 'md',
+    theme: {
+        primary: '#E8E8E8',
+        success: '#009944',
+        info: '#63c0df',
+        warning: '#f0541e',
+        error: '#cf000f'
+    }
 });
