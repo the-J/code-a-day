16c26a2d32fc166f1a154856ba797ecc7b4a7524
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-29 19:13:19 +0100

    sorting projects list

--- vue/vuetify/src/views/Dashboard.vue
+++ vue/vuetify/src/views/Dashboard.vue
@@ -3,6 +3,18 @@
         <h1 class="subheading grey--text">Dashboard</h1>
 
         <v-container class='my-5'>
+            <v-layout row class='mb-3'>
+                <v-btn small flat color="grey" @click="sortBy('title')">
+                    <v-icon left small>folder</v-icon>
+                    <span class="caption text-lowercase"> by project name</span>
+                </v-btn>
+
+                <v-btn small flat color="grey" @click="sortBy('person')">
+                    <v-icon left small>person</v-icon>
+                    <span class="caption text-lowercase"> by person</span>
+                </v-btn>
+            </v-layout>
+
             <v-card flat v-for="project in projects" :key="project.id">
                 <v-layout row wrap :class="`pa-3 project ${project.status}`">
                     <v-flex xs12 md6>
@@ -27,7 +39,9 @@
                         </div>
                     </v-flex>
                 </v-layout>
+
                 <v-divider></v-divider>
+
             </v-card>
         </v-container>
     </div>
@@ -38,18 +52,45 @@
         data() {
             return {
                 projects: [
-                    { id: 1, title: 'Project One', person: 'Person One', dueDate: '1 Feb 2020', status: 'ongoing' },
-                    { id: 1, title: 'Project Two', person: 'Person Two', dueDate: '15 Jan 2018', status: 'complete' },
                     {
                         id: 1,
+                        title: 'Project One',
+                        person: 'Person One',
+                        dueDate: '1 Feb 2020',
+                        status: 'ongoing'
+                    },
+                    {
+                        id: 2,
+                        title: 'Project Two',
+                        person: 'Person Two',
+                        dueDate: '15 Jan 2018',
+                        status: 'complete'
+                    },
+                    {
+                        id: 3,
                         title: 'Project Three',
                         person: 'Person Three',
                         dueDate: '21 Apr 2020',
                         status: 'overdue'
                     },
-                    { id: 1, title: 'Project Four', person: 'Person Four', dueDate: '10 Dec 2015', status: 'complete' }
+                    {
+                        id: 4,
+                        title: 'Project Four',
+                        person: 'Person Four',
+                        dueDate: '10 Dec 2015',
+                        status: 'complete'
+                    }
                 ]
             };
+        },
+        methods: {
+            sortBy( param ) {
+                this.projects.sort(( a, b ) => {
+                    if (a[ param ] > b[ param ]) return 1;
+                    else if (a[ param ] < b[ param ]) return -1;
+                    else return 0;
+                });
+            }
         }
     };
 </script>
