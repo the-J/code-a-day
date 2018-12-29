d6644b21ff3e272b3ea90f182426f398752000e2
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-29 19:16:31 +0100

    sorting button tooltips

--- vue/vuetify/src/views/Dashboard.vue
+++ vue/vuetify/src/views/Dashboard.vue
@@ -4,15 +4,23 @@
 
         <v-container class='my-5'>
             <v-layout row class='mb-3'>
-                <v-btn small flat color="grey" @click="sortBy('title')">
-                    <v-icon left small>folder</v-icon>
-                    <span class="caption text-lowercase"> by project name</span>
-                </v-btn>
+                <v-tooltip top>
+                    <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+                        <v-icon left small>folder</v-icon>
+                        <span class="caption text-lowercase"> by project name</span>
+                    </v-btn>
 
-                <v-btn small flat color="grey" @click="sortBy('person')">
-                    <v-icon left small>person</v-icon>
-                    <span class="caption text-lowercase"> by person</span>
-                </v-btn>
+                    <span>Sort Projects by project name</span>
+                </v-tooltip>
+
+                <v-tooltip top>
+                    <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+                        <v-icon left small>person</v-icon>
+                        <span class="caption text-lowercase"> by person</span>
+                    </v-btn>
+
+                    <span> Sort projects by person</span>
+                </v-tooltip>
             </v-layout>
 
             <v-card flat v-for="project in projects" :key="project.id">
