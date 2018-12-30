8e6ceaab104b6e43fb94a760fb26157c47bbe6aa
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-12-30 12:24:19 +0100

    initiating d connection in fb

--- vue/vuetify/src/fb.js
+++ vue/vuetify/src/fb.js
@@ -3,11 +3,17 @@ import 'firebase/firestore';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBZT6oZEKPsuieYrQxoFk9tQL3bqKktcX4",
-    authDomain: "vuetify-8e1bc.firebaseapp.com",
-    databaseURL: "https://vuetify-8e1bc.firebaseio.com",
-    projectId: "vuetify-8e1bc",
-    storageBucket: "vuetify-8e1bc.appspot.com",
-    messagingSenderId: "1077754234292"
+    apiKey: 'AIzaSyBZT6oZEKPsuieYrQxoFk9tQL3bqKktcX4',
+    authDomain: 'vuetify-8e1bc.firebaseapp.com',
+    databaseURL: 'https://vuetify-8e1bc.firebaseio.com',
+    projectId: 'vuetify-8e1bc',
+    storageBucket: 'vuetify-8e1bc.appspot.com',
+    messagingSenderId: '1077754234292'
 };
 firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+db.settings({ timestampsInSnapshots: true });
+
+export default db;
