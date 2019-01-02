d22fec37ab9ba5c0b15b087f3239fbf3889a4b50
thej <24.Pomarancze/>
2019-01-02 21:30:17 +0100

    testing wit mocha and chai

--- tests/mochatest/app.js
+++ tests/mochatest/app.js
@@ -1,3 +1,12 @@
-module.exports = function() {
+module.exports = {
+   sayHello,
+   addNumbers
+}
+
+function sayHello() {
    return 'hello';
+}
+
+function addNumbers (a, b) {
+   return a + b;
 }
--- tests/mochatest/test/app.test.js
+++ tests/mochatest/test/app.test.js
@@ -1,8 +1,29 @@
 const assert = require('chai').assert;
 const app = require('../app');
 
+const sayHello =  require('../app').sayHello;
+const addNumbers =  require('../app').addNumbers;
+
 describe('App', function() {
-   it('should return hello', function() {
-      assert.equal(app(), 'hello');
+   // sayHello
+   it('"sayHello" function should return "hello"', function() {
+      let result = sayHello();
+      assert.equal(result, 'hello');
+   })
+
+   it('"sayHello" should return type string', function() {
+      let result = sayHello();
+      assert.typeOf(result, 'string');
+   })
+
+   // addNumbers
+   it('"addNum(2, 3)" should return 5', function() {
+      let result = addNumbers(2,3);
+      assert.equal(result, 5);
+   })
+
+   it('"addNum(X, X)" should return number', function() {
+      let result = addNumbers(2,3);
+      assert.typeof(result, 'number');
    })
 })
