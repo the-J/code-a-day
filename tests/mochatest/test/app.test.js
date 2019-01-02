57e9f803534123a54da418ebd025affd79174b69
thej <24.Pomarancze/>
2019-01-02 21:08:18 +0100

    init mocha test

---
+++ tests/mochatest/test/app.test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const app = require('../app');
+
+describe('App', function() {
+   it('should return hello', function() {
+      assert.equal(app(), 'hello');
+   })
+})
