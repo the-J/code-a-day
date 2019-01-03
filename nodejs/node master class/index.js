107f095bfbd68860993da26bc3f5697b313dc04d
thej <24.Pomarancze/>
2019-01-03 13:21:35 +0100

    basic http post req validation wit Joi

--- nodejs/node master class/index.js
+++ nodejs/node master class/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
+const Joi = require('joi');
+const express = require('express');
 const app = express();
 
 // Middleware for parsing body by default
-app.use(express.json);
+app.use(express.json());
 
 const cars = [
    {id: 1, name: 'car1'},
@@ -14,24 +15,38 @@ const cars = [
 app.get('/', (req, res) => {
    res.send('Hello world!!!');
    res.end();
-});
+ });
 
 app.get('/api/cars/', (req, res) => {
    res.send(cars);
    res.end();
-})
+ })
 
 app.get('/api/cars/:id', (req, res) => {
-   const car = cars.find(car => car.id === parseInt(req.params.id))
+   const car = cars.find(car => car.id === parseInt(req.params.id));
    
-   if(!car) res.status(404).send('No car found');
+   if (!car) res.status(404).send('No car found');
 
    res.send(car);
-   res.end();
-});
+ });
 
 // POST methods
-app.post('/api/car', (re, res) => {
+app.post('/api/car', (req, res) => {
+   
+   console.log(req.body);
+
+   const schema = {
+      name: Joi.string().min(3).required()
+   }
+
+   const result = Joi.validate(req.body, schema);
+   console.log(result);
+  
+   if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+      return;
+   }
+   
    const car = {
       id: cars.length + 1,
       name: req.body.name
