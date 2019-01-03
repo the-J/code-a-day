19642e0df44d50536688f50138e9f25b122854df
thej <24.Pomarancze/>
2019-01-03 16:29:18 +0100

    delete car method; clearing code

--- nodejs/node master class/index.js
+++ nodejs/node master class/index.js
@@ -5,40 +5,42 @@ const app = express();
 // Middleware for parsing body by default
 app.use(express.json());
 
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`App listening on port ${port}`));
+
 const cars = [
-   {id: 1, name: 'car1'},
-   {id: 2, name: 'car2'},
-   {id: 3, name: 'car3'}
+   { id: 1, name: 'car1' },
+   { id: 2, name: 'car2' },
+   { id: 3, name: 'car3' }
 ]
 
-// GET methods
+/**
+ * GET methods
+ */
 app.get('/', (req, res) => {
    res.send('Hello world!!!');
    res.end();
- });
+});
 
 app.get('/api/cars/', (req, res) => {
    res.send(cars);
    res.end();
- })
+})
 
 app.get('/api/cars/:id', (req, res) => {
    const car = cars.find(car => car.id === parseInt(req.params.id));
-   
-   if (!car) res.status(404).send('No car found');
-
+   if (!car) return res.status(404).send('No car found');
    res.send(car);
- });
+});
 
-// POST methods
+/**
+ * POST methods
+ */
 app.post('/api/car', (req, res) => {
-   const {error} = validateCar(req.body);
-  
-   if (error) {
-      res.status(400).send(error.details[0].message);
-      return;
-   }
-   
+   const { error } = validateCar(req.body);
+
+   if (error) return res.status(400).send(error.details[0].message);
+
    const car = {
       id: cars.length + 1,
       name: req.body.name
@@ -48,40 +50,53 @@ app.post('/api/car', (req, res) => {
    res.send(car);
 });
 
-// PUT methods
+/**
+ * PUT methods
+ */
 app.put('/api/car/:id', (req, res) => {
-   console.log(req.params.id)
    // Find car
    const car = cars.find(car => car.id === parseInt(req.params.id));
 
    // If don't exists, return 404
-   if(!car) {
-      res.status(404).send('No car found');
-      return;
-   };
+   if (!car) return res.status(404).send('No car found');
 
    // Validate input data
-   const {error} = validateCar(req.body);
-   
-   // If invalid, send 400 - bda req
-   if (error) {
-      res.tatus(400).send(error.details[0].message);
-      return;
-   }
+   const { error } = validateCar(req.body);
+
+   // If invalid, send 400 - bad req
+   if (error) return res.status(400).send(error.details[0].message);
 
    // Update car
    car.name = req.body.name;
+   
    // return updated car
    res.send(car);
 })
 
-// PORT
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`App listening on port ${port}`));
+/**
+ * DELETE methods
+ */
+app.delete('/api/cars/:id', (req, res) => {
+   const car = cars.find(c => c.id === parseInt(req.params.id));
+   if (!car) res.status(404).send('No car with given ID');
 
-// HELPERS
+   // delete
+   const index = cars.indexOf(car)
+   cars.splice(index, 1);
 
-// validate car name
+   res.send(cars);
+})
+
+/**
+ * HELPERS
+ */
+
+/**
+ * Validate if car has a name
+ * 
+ * @param {Object} car
+ * @param {Object.String} car.name 
+ */
 function validateCar(car) {
    const schema = {
       name: Joi.string().min(3).required()
