64b40cd8e4671daed442e3ca1cc83ee761dbfadb
thej <24.Pomarancze/>
2019-01-03 12:23:08 +0100

    diff basic enpoints

--- nodejs/node master class/index.js
+++ nodejs/node master class/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express();
 
+const cars = [
+   {id: 1, name: 'car1'},
+   {id: 2, name: 'car2'},
+   {id: 3, name: 'car3'}
+]
+
 app.get('/', (req, res) => {
    res.send('Hello world!!!');
+   res.end();
+});
+
+app.get('/api/cars/', (req, res) => {
+   res.send(cars);
+   res.end();
+})
+
+app.get('/api/cars/:id', (req, res) => {
+   const car = cars.find(car => car.id === parseInt(req.params.id))
+   
+   if(!car) res.status(404).send('No car found');
+   
+   res.send(car);
+   res.end();
 });
 
 const port = process.env.PORT || 3000
