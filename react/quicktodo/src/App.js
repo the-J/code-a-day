24458900c9cb510cb7ddb54179b3df5c19cd7732
thej <24.Pomarancze/>
2019-01-04 20:35:28 +0100

    cleaning and init data

--- react/quicktodo/src/App.js
+++ react/quicktodo/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
- import './App.css';
+
+import TodoList from './components/TodoList';
+
+import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take trash',
+        completed: false
+      },{
+        id: 2,
+        title: 'Wash car',
+        completed: false
+      },{
+        id: 3,
+        title: 'Call mom',
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-         <h1>App</h1>
+        <TodoList 
+          todo={this.state.todos}
+        />
       </div>
     );
   }
---
+++ react/quicktodo/src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+   render() {
+      return (
+         <div>
+            <h1>Todo</h1>
+            <ul>
+               <li>Add Todo</li>
+               <li>List of todos</li>
+            </ul>
+         </div>
+      )
+   }
+}
+
+export default TodoList;
