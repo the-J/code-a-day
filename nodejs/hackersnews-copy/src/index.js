ebbe60e1da9decaa6092135559263d1a193412bb
thej <24.Pomarancze/>
2019-01-07 14:42:09 +0100

    restructuring project

--- nodejs/hackersnews-copy/src/index.js
+++ nodejs/hackersnews-copy/src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-   info: String!
-   feed: [Link!]!
-}
-
-type Link {
-   id: ID!
-   description: String!
-   url: String!
-}
-`
-
 const links = [
    {
       id: 'link-0',
@@ -34,7 +21,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-   typeDefs,
+   typeDefs: 'src/schema.graphql',
    resolvers
 });
 
