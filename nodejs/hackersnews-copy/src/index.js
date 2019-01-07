35440a289ac63026d2ed01c4261fe1c17215574c
thej <24.Pomarancze/>
2019-01-07 14:38:22 +0100

    extending API

--- nodejs/hackersnews-copy/src/index.js
+++ nodejs/hackersnews-copy/src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
 type Query {
    info: String!
+   feed: [Link!]!
+}
+
+type Link {
+   id: ID!
+   description: String!
+   url: String!
 }
 `
 
+const links = [
+   {
+      id: 'link-0',
+      url: 'www.howtographql.com',
+      description: 'GraphQL tutorial'
+   }
+];
+
 const resolvers = {
    Query: {
-      info: () => `This is API for hackernews clone`
+      info: () => `This is API for hackernews clone`,
+      feed: () => links
+   },
+   Link: {
+      id: parent => parent.id,
+      url: parent => parent.url,
+      description: parent => parent.description
    }
 }
 
