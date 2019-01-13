4c0fd69fe1e9bbcd89e526ea2074433df7c10e4f
thej <24.Pomarancze/>
2019-01-13 18:06:58 +0100

    basic class binding

--- angular/beginnersCrashCourse/src/app/home/home.component.ts
+++ angular/beginnersCrashCourse/src/app/home/home.component.ts
@@ -5,8 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
+  h1GreyStyle: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
@@ -14,5 +17,6 @@ export class HomeComponent implements OnInit {
 
   someClick() {
     console.log('someClick clicked');
+    this.h1GreyStyle = true;
   }
 }
