2898ff04b17c51e118c110e8bfd18b0b92442ef8
thej <24.Pomarancze/>
2019-01-17 10:34:36 +0100

    validation update

--- nodejs/movieGenres/routes/customers.js
+++ nodejs/movieGenres/routes/customers.js
@@ -64,7 +64,7 @@ router.delete('/:id', async (req, res) => {
  */
 function validateCustomer(customer) {
    const schema = {
-      name: Joi.string().min(3).required()
+      name: Joi.string().min(5).required()
    };
 
    return Joi.validate(customer, schema);
