8c1e0b2f3e4464ec028fb55cbf5693c52d59bd21
thej <24.Pomarancze/>
2019-01-19 14:52:30 +0100

    movie model

---
+++ nodejs/movieGenres/models/movie.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const { genreSchema } = require('./genre');
+
+const Movie = mongoose.model('Customer', new mongoose.Schema({
+   title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255
+   },
+   genre: {
+      type: genreSchema,
+      required: true
+   },
+   numberInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+      max: 255
+   },
+   dailyRentalRate: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+      max: 255
+   }
+}));
+
+/**
+ * HELPERS
+ */
+
+/**
+ * @param {String} movie 
+ */
+function validateMovie(movie) {
+   const schema = {
+      title: Joi.string().min(5).max(255).required(),
+      genreId: Joi.string().required(),
+      numberInStock: Joi.number().min(0).required(),
+      dailyRentalRate: Joi.number().min(0).required()
+   };
+
+   return Joi.validate(movie, schema);
+}
+
+exports.Movie = Movie;
+exports.validateMovie = validateMovie;
