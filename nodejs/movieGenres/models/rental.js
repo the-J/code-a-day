04884ccdaa9ee5f13d9e8559bf4611b2c31c7664
thej <24.Pomarancze/>
2019-01-19 16:12:05 +0100

    rental model

---
+++ nodejs/movieGenres/models/rental.js
@@ -0,0 +1,85 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+// creating new customer schema - we don't need all his info.
+// same goes to movies
+const customerSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+   },
+   phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+   },
+   isGold: {
+      type: Boolean,
+      required: true
+   },
+});
+
+const moviesSchema = new mongoose.Schema({
+   title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255
+   },
+   dailyRentalRate: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+      max: 255
+   }
+});
+
+const rentalSchema = new mongoose.Schema({
+   customer: {
+      type: customerSchema,
+      required: true
+   },
+   movie: {
+      type: moviesSchema,
+      required: true
+   },
+   dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now
+   },
+   dateReturned: {
+      typ: Date
+   },
+   rentalFee: {
+      type: Number,
+      min: 0
+   }
+});
+
+
+/**
+ * HELPERS
+ */
+
+/**
+ * @param {String} rental 
+ */
+function validateRntal(rental) {
+   const schema = {
+      customerId: Joi.string().required(),
+      movieId: Joi.string().required()
+   };
+
+   return Joi.validate(rental, schema);
+}
+
+const Rental = mongoose.model('Rental', rentalSchema)
+
+exports.validateRntal = validateRntal;
+exports.Rental = Rental;
