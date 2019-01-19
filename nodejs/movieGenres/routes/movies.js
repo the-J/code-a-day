b2ca9d65771d617ef34da7e9050d335a022dc676
thej <24.Pomarancze/>
2019-01-19 14:59:54 +0100

    movies api routes

---
+++ nodejs/movieGenres/routes/movies.js
@@ -0,0 +1,62 @@
+const express = require('express')
+// diff then in index.js - need to work with 'instance' of express
+const router = express.Router()
+
+const { Movie, validateMovie } = require('../models/movie')
+const { Genre } = require('../models/genre')
+
+router.get('/', async (req, res) => {
+   const movies = await Movie.find().sort('name');
+   res.send(movies);
+});
+
+router.get('/:id', async (req, res) => {
+   const movie = await Movie.findById(req.params.id);
+   if (!movie) return res.status(404).send('Not found');
+   res.send(movie);
+});
+
+router.post('/', async (req, res) => {
+   const { error } = validateMovie(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   const genre = await Genre.findById(re.body.genreId);
+   if (!genre) return res.status(400).send('Invalid genre mate');
+
+   let movie = new Movie({
+      title: req.body.title,
+      gere: {
+         _id: genre._id,
+         name: genre.name
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+   });
+
+   // reassigning value - id returned from save operation
+   movie = await movie.save();
+   res.send(movie);
+});
+
+router.put('/:id', async (req, res) => {
+   const { error } = validateMovie(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   const movie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      { title: req.body.title },
+      { new: true }
+   )
+
+   if (!movie) return res.status(404).send('Not found.');
+
+   res.send(movie);
+});
+
+router.delete('/:id', async (req, res) => {
+   const movie = await Movie.findByIdAndRemove(req.params.id);
+   if (!movie) return res.status(404).send('Not found.');
+   res.send(movie);
+});
+
+module.exports = router;
