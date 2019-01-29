f6f1d9d35e59dd9ff827001ba658a19846d5776d
thej <24.Pomarancze/>
2019-01-29 09:45:03 +0100

    Example re + typos

--- nodejs/movieGenres/routes/movies.js
+++ nodejs/movieGenres/routes/movies.js
@@ -5,6 +5,16 @@ const router = express.Router()
 const { Movie, validateMovie } = require('../models/movie')
 const { Genre } = require('../models/genre')
 
+/**
+ * Example req:
+ * {
+	   "title": "Mad Max",
+   	"genreId": "5c500f8d04de2843e66bfc52", <- id from genre collection
+	   "numberInStock": 10,
+	   "dailyRentalRate": 10
+   }
+ */
+
 router.get('/', async (req, res) => {
    const movies = await Movie.find().sort('name');
    res.send(movies);
@@ -20,12 +30,12 @@ router.post('/', async (req, res) => {
    const { error } = validateMovie(req.body);
    if (error) return res.status(400).send(error.details[0].message);
 
-   const genre = await Genre.findById(re.body.genreId);
+   const genre = await Genre.findById(req.body.genreId);
    if (!genre) return res.status(400).send('Invalid genre mate');
 
    let movie = new Movie({
       title: req.body.title,
-      gere: {
+      genre: {
          _id: genre._id,
          name: genre.name
       },
