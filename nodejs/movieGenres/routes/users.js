2aa08bc08f44f02a1e23178fb1db917c52e0f8b1
thej <24.Pomarancze/>
2019-01-31 10:59:01 +0100

    updating user creation to use lodash

--- nodejs/movieGenres/routes/users.js
+++ nodejs/movieGenres/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const _ = require('lodash');
 // diff then in index.js - need to work with 'instance' of express
 const { User, validateUser } = require('../models/user')
 const router = express.Router()
@@ -14,17 +15,28 @@ router.post('/', async (req, res) => {
    const { error } = validateUser(req.body);
    if (error) return res.status(400).send(error.details[0].message);
 
+   // email needs to be unique - so user is already in db if email exists
    let user = await User.findOne({ email: req.body.email })
    if (user) return res.status(400).send('User already registered');
 
-   user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-   });
+   // updating construction to lodash
+   // user = new User({
+   //    name: req.body.name,
+   //    email: req.body.email,
+   //    password: req.body.password
+   // });
+   user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
    await user.save();
-   res.send(user);
+
+   // dont send password
+   // res.send({
+   //    name: user.name,
+   //    email: user.email
+   // });
+
+   // dont return pass
+   res.send(_.pick(user, ['_id', 'name', 'email']))
 });
 
 module.exports = router;
