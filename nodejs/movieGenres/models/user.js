f53ef0a9b24c200ac3355761f7dce7ba92931839
thej <24.Pomarancze/>
2019-01-31 13:29:45 +0100

    extra password validation in user schema

--- nodejs/movieGenres/models/user.js
+++ nodejs/movieGenres/models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const PasswordComplexity = require('joi-password-complexity');
+
+const passwordComplexityOptions = {
+   min: 5,
+   max: 1024,
+   lowerCase: 1,
+   upperCase: 1,
+   numeric: 1,
+   symbol: 1,
+   requirementCount: 2,
+}
 
 const userSchema = new mongoose.Schema({
    name: {
       type: String,
+      trim: true,
       minlength: 5,
       maxlength: 50,
       required: true
@@ -11,14 +23,15 @@ const userSchema = new mongoose.Schema({
    email: {
       type: String,
       unique: true,
+      trim: true,
       minlength: 5,
       maxlength: 255,
       required: true
    },
    password: {
       type: String,
-      minlength: 5,
-      maxlength: 1024,
+      minlength: passwordComplexityOptions.min,
+      maxlength: passwordComplexityOptions.max,
       required: true
    }
 });
@@ -29,7 +42,7 @@ function validateUser(user) {
    const schema = {
       name: Joi.string().min(5).max(50).required(),
       email: Joi.string().min(5).max(255).email().required(),
-      password: Joi.string().min(5).max(255).required(),
+      password: new PasswordComplexity(passwordComplexityOptions)
    }
 
    return Joi.validate(user, schema);
