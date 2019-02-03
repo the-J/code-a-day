7551d834228c260a1db49ff5b00f1e8570afae7c
thej <24.Pomarancze/>
2019-02-03 09:51:20 +0100

    authentication middleware

---
+++ nodejs/movieGenres/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+function auth(req, res, next) {
+   const token = req.header('x-auth-token');
+   if (!token) return res.status(401).send('No token provided');
+
+   try {
+      // return decoded payload or exception
+      req.user = jwt.verify(token, config.get('jwtPrivateKey'));
+      next();
+   }
+   catch (ex) {
+      res.status(400).send('Invalid auth token');
+   }
+}
+
+module.exports = auth;
